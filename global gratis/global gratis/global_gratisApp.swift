00563040c5417ac31a74533e5fb328d88df66e0a
Daniel8473 <Daniel_8473@hotmail.com>
2021-01-16 16:24:03 -0800

    nothing important
    removed read me file to test something

---
+++ global gratis/global gratis/global_gratisApp.swift
@@ -0,0 +1,17 @@
+//
+//  global_gratisApp.swift
+//  global gratis
+//
+//  Created by Daniel Silva on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct global_gratisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
