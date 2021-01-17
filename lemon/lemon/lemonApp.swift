d3025ceb47f3e13a356bf79774d0043db3c97ed9
Daniel8473 <Daniel_8473@hotmail.com>
2021-01-16 20:28:26 -0800

    new project

---
+++ lemon/lemon/lemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  lemonApp.swift
+//  lemon
+//
+//  Created by Daniel Silva on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct lemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
