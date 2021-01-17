687535a6ba83eaf809e51decbd74c96a2d83b184
Soumya Duriseti <soumyaduriseti@gmail.com>
2021-01-16 18:53:51 -0800

    Firebase ACTUALLY works now i hope

--- global gratis/global gratis/global_gratisApp.swift
+++ global gratis/global gratis/global_gratisApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct global_gratisApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
